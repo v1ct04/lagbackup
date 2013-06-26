bee130a0d4d70fdf3b19b4a4f96ae213de2e221f
Victor Elias <victorgelias@gmail.com>
2013-06-26 02:30:33 -0300

    Adding serialVersionUID to BackupException
    Eclipse kept complaining about it and it was the only warning being
    raised.

--- src/com/v1ct04/ces22/lagbackup/backup/exception/BackupException.java
+++ src/com/v1ct04/ces22/lagbackup/backup/exception/BackupException.java
@@ -2,6 +2,8 @@ package com.v1ct04.ces22.lagbackup.backup.exception;
 
 public class BackupException extends Exception {
 
+    private static final long serialVersionUID = 7806354171295813051L;
+
     public BackupException(String message) {
         super(message);
     }
